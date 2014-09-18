f1bf37dd8d9bc96f83dd4047012a67940c2b5da4
Alex <alexandre.frantz@telecomnancy.net>
2014-09-18 03:08:42 +0200

    mail

--- src/server/mail/mailer.js
+++ src/server/mail/mailer.js
@@ -5,30 +5,65 @@ var path           = require('path');
 var emailTemplates = require('email-templates');
 var fs = require('fs');
 
+var logger = require(__dirname+'/../../logger/logger');
 
-// create reusable transporter object using SMTP transport
-var transporter = nodemailer.createTransport(smtpTransport({
+var MAIL_CONFIG_FILE_PATH = process.cwd() + '/config/mail.json',
+    mailOptions;
 
-}));
+var pathlang,
+    pathUserModule;
 
+// reusable transporter object using SMTP transport
+var transporter;
 
 module.exports = {
     send: function(to, subject, html, text, cb) {
 
-        var mailOptions = {
-            from: 'Songpeek ☊ <contact@songpeek.com>', // sender address
+        if (!transporter) {
+            // Check if config file for database exists
+            try {
+                mailOptions = JSON.parse(fs.readFileSync(MAIL_CONFIG_FILE_PATH));
+            }
+            catch (err) {
+                logger.error(new Error(MAIL_CONFIG_FILE_PATH + ' doesn\'t exists or it\'s not a valid JSON.'), 'Mailer');
+                process.exit(1);
+            }
+
+            if (!mailOptions || !mailOptions.port || !mailOptions.host || !mailOptions.from) {
+                logger.error(new Error('mail config file is invalid.'), 'Mailer');
+                process.exit(1);
+            }
+
+            var transporter = nodemailer.createTransport(smtpTransport({
+                port: mailOptions.port,
+                host: mailOptions.host,
+                auth: {
+                    user: mailOptions.auth.user,
+                    pass: mailOptions.auth.pass
+                },
+                secure: true,
+                maxConnections: 5,
+                maxMessages: 10
+            }));
+        }
+
+        var optionsForTransporter = {
+            from: mailOptions.from, // sender address
             to: to, // list of receivers
             subject: subject,
             html: html,
             text: text
         };
 
-        transporter.sendMail(mailOptions, function(err, info){
+        transporter.sendMail(optionsForTransporter, function(err, info){
             cb(err, info);
         });
     },
     mail: function(mail, templateName, module, lang, params, cb) {
         var $this = this;
+        if (!pathlang) {
+
+        }
         params.i18n = require(__dirname+'/i18n/'+lang);
         emailTemplates(__dirname+'/../'+module+'/mail', function(err, template) {
             if(err) return cb(err);
