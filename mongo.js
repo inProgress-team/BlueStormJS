bd99d99e6f7a2420e2e4d95d22b8f5c726fd5193
Alex <alexandre.frantz@telecomnancy.net>
2014-10-07 14:18:55 +0200

    mongo

--- mongo.js
+++ mongo.js
@@ -24,8 +24,8 @@ module.exports = function(callback) {
             if (err)
                 return callback(err);
 
-            mongo = res;
-            return callback(res);
+            mongo = res.db;
+            return callback(res.db);
         });
     }
     else {
@@ -33,8 +33,8 @@ module.exports = function(callback) {
             if (err)
                 return callback(err);
 
-            mongo = res;
-            return callback(res);
+            mongo = res.db;
+            return callback(res.db);
         });
     }
 };
