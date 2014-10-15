28e35118cb5a7456edcb0e54dd78c2b0bdbfb839
Alex <alexandre.frantz@telecomnancy.net>
2014-10-15 21:42:38 +0200

    save models for mongoose before sending db instance

--- index.js
+++ index.js
@@ -1,21 +1,59 @@
-var async = require('async');
+var async = require('async'),
+    events = require('events'),
+    eventEmitter = new events.EventEmitter();
+    eventEmitter.setMaxListeners(100);
 
 var logger = require(__dirname+'/src/logger/logger'),
     server = require(__dirname+'/src/server/server'),
     cli = require(__dirname+'/src/cli'),
     arborescence = require(__dirname+'/src/arborescence');
 
+var firstCall = true,
+    modelsLoaded = false;
+
 module.exports = {
     cli: cli,
     server: server,
     logger: logger,
     db: function(callback) {
-        require(__dirname + '/db')(function(err, res) {
-            if (err)
-                return callback(err);
+        if (firstCall) {
+            firstCall = false;
+            require(__dirname + '/db')(function(err, res) {
+                if (err)
+                    return callback(err);
 
-            return callback(res.db);
-        });
+                if (res.type == 'mongoose') {
+                    arborescence.getRequiredFiles('models', function (files) {
+                        arborescence.loadFiles(files, null, function () {
+                            eventEmitter.emit('bluestorm:modelsLoaded');
+                            return callback(res.db);
+                        });
+                    });
+                }
+            });
+        }
+        else {
+            if (modelsLoaded) {
+                require(__dirname + '/db')(function(err, res) {
+                    if (err)
+                        return callback(err);
+
+                    return callback(res.db);
+                });
+            }
+            else {
+                eventEmitter.on('bluestorm:modelsLoaded', function() {
+                    modelsLoaded = true;
+                    require(__dirname + '/db')(function(err, res) {
+                        if (err)
+                            return callback(err);
+
+                        return callback(res.db);
+                    });
+                });
+            }
+        }
+
     },
     mongo: function(callback) {
         require(__dirname + '/mongo')(callback);
@@ -23,12 +61,3 @@ module.exports = {
     user: require(__dirname + '/src/server/user/models/user'),
     mailer: require(__dirname + '/src/email/mailer')
 };
-
-require(__dirname + '/db')(function(err, res) {
-    if (res.type == 'mongoose') {
-        arborescence.getRequiredFiles('models', function (files) {
-            arborescence.loadFiles(files, null, function () {
-            });
-        });
-    }
-});
