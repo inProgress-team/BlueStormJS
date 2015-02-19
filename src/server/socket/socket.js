6eb6928ee78141cfa3a914f1235dd7bac9fa0d14
Alex <alexandre.frantz@telecomnancy.net>
2015-02-19 22:42:12 +0100

    sticky sockets

--- src/server/socket/socket.js
+++ src/server/socket/socket.js
@@ -12,6 +12,9 @@ var logger = require(__dirname+'/../../logger/logger'),
 var ROLES_CONFIG_FILE_PATH = process.cwd() + '/config/roles.json';
 var rolesConfig;
 
+var cacheFiles;
+var config;
+
 var roleContainsRole = function(activeRole, requiredRole, callback) {
     if (!rolesConfig || rolesConfig.roles.length == 0)
         return callback(ROLES_CONFIG_FILE_PATH + ' is empty');
@@ -105,98 +108,188 @@ var checkAuthentification = function(data, callback) {
     }
 };
 
+var onConnect = function(socket) {
+    /**
+     * Override socket.on
+     */
+    socket.onAux = socket.on;
+    socket.on = function (url, options, userCallback) {
+        socket.onAux(url, function (data, callback) {
+            if (typeof data == 'function') {
+                callback = data;
+                data = {
+                    data: {}
+                };
+            } else if (!data) {
+                data = {
+                    data: {}
+                };
+            }
+            if (config.debug) {
+                logger.log('SOCKET : ' + url);
+            }
+            user.tokenIsValid(data.token, function (err, user) {
+                if (!err && user)
+                    data.user = user;
 
-module.exports = function(config) {
-    if (!config) {
-        config = {};
-    }
-    var cacheFiles = null;
+                if (!data.data)
+                    data.data = {};
+                if (typeof options == 'object' && (options.authentification || options.roles)) {
+                    if (options.roles)
+                        data.roles = options.roles;
 
-    var io;
-    var app = express();
-    var server = http.createServer(app);
-    io = socket(server);
-    io.set('transports', [
-        'websocket'
-        , 'flashsocket'
-        , 'htmlfile'
-        , 'xhr-polling'
-        , 'jsonp-polling'
-    ]);
-    var d = domain.create();
+                    checkAuthentification(data, function (err) {
+                        if (err)
+                            return callback(err);
 
-    d.on('error', function(err) {
-        logger.error(err, 'Socket.io'+':'+config.port);
-    });
-
-    d.run(function() {
-        io.adapter(redis({ host: 'localhost', port: 6379 }));
-        io.on('connection', function (socket) {
-            /**
-             * Override socket.on
-             */
-            socket.onAux = socket.on;
-            socket.on = function(url, options, userCallback) {
-                socket.onAux(url, function(data, callback) {
-                    if (typeof data == 'function') {
-                        callback = data;
-                        data = {
-                            data: {}
-                        };
-                    } else if (!data) {
-                        data = {
-                            data: {}
-                        };
-                    }
-                    if(config.debug) {
-                        logger.log('SOCKET : ' + url);
-                    }
-                    user.tokenIsValid(data.token, function(err, user) {
-                        if (!err && user)
-                            data.user = user;
-
-                        if (!data.data)
-                            data.data = {};
-                        if (typeof options == 'object' && (options.authentification || options.roles)) {
-                            if (options.roles)
-                                data.roles = options.roles;
-
-                            checkAuthentification(data, function(err) {
-                                if (err)
-                                    return callback(err);
-
-                                return userCallback(data, callback);
-                            });
-                        }
-                        else {
-                            return options(data, callback);
-                        }
+                        return userCallback(data, callback);
                     });
+                }
+                else {
+                    return options(data, callback);
+                }
+            });
+        });
+    };
+
+    if (!cacheFiles) {
+        arborescence.getRequiredFiles('socket', function (files) {
+            cacheFiles = files;
+            arborescence.loadFiles(cacheFiles, socket);
+        });
+    } else {
+        arborescence.loadFiles(cacheFiles, socket);
+    }
+};
+
+module.exports = function(c) {
+    config = c || {};
+
+    if (process.env.NODE_ENV == 'production') {
+        var express = require('express'),
+            cluster = require('cluster'),
+            net = require('net'),
+            sio = require('socket.io'),
+            sio_redis = require('socket.io-redis');
+
+        var port = config.port,
+            num_processes = require('os').cpus().length;
+
+        if (cluster.isMaster) {
+            // This stores our workers. We need to keep them to be able to reference
+            // them based on source IP address. It's also useful for auto-restart,
+            // for example.
+            var workers = [];
+
+            // Helper function for spawning worker at index 'i'.
+            var spawn = function (i) {
+                workers[i] = cluster.fork();
+
+                // Optional: Restart worker on exit
+                workers[i].on('exit', function (worker, code, signal) {
+                    console.log('respawning worker', i);
+                    spawn(i);
                 });
             };
 
-            if (!cacheFiles) {
-                arborescence.getRequiredFiles('socket', function (files) {
-                    cacheFiles = files;
-                    arborescence.loadFiles(cacheFiles, socket);
-                });
-            } else {
-                arborescence.loadFiles(cacheFiles, socket);
+            // Spawn workers.
+            for (var i = 0; i < num_processes; i++) {
+                spawn(i);
             }
 
-        });
-    });
+            // Helper function for getting a worker index based on IP address.
+            // This is a hot path so it should be really fast. The way it works
+            // is by converting the IP address to a number by removing the dots,
+            // then compressing it to the number of slots we have.
+            //
+            // Compared against "real" hashing (from the sticky-session code) and
+            // "real" IP number conversion, this function is on par in terms of
+            // worker index distribution only much faster.
+            var worker_index = function (ip, len) {
+                var s = '';
+                for (var i = 0, _len = ip.length; i < _len; i++) {
+                    if (ip[i] !== '.') {
+                        s += ip[i];
+                    }
+                }
 
-    if(config.debug && cluster.isMaster) {
-        logger.log('Sockets', ['green'], ' listening on port ', config.port, ['yellow'], '.');
-    }
+                return Number(s) % len;
+            };
 
-    if (process.env.NODE_ENV != 'production') {
-        io.listen(config.port, function() {
-            return server;
-        });
+            // Create the outside facing server listening on our port.
+            var server = net.createServer(function (connection) {
+                // We received a connection and need to pass it to the appropriate
+                // worker. Get the worker for this connection's source IP and pass
+                // it the connection.
+                var worker = workers[worker_index(connection.remoteAddress, num_processes)];
+                worker.send('sticky-session:connection', connection);
+            }).listen(port);
+
+            if(config.debug) {
+                logger.log('Sockets', ['green'], ' listening on port ', config.port, ['yellow'], '.');
+            }
+        } else {
+            // Note we don't use a port here because the master listens on it for us.
+            var app = new express();
+
+            // Here you might use middleware, attach routes, etc.
+
+            // Don't expose our internal server to the outside.
+            var server = app.listen(0, 'localhost'),
+                io = sio(server);
+
+            io.on('connection', onConnect);
+
+            // Tell Socket.IO to use the redis adapter. By default, the redis
+            // server is assumed to be on localhost:6379. You don't have to
+            // specify them explicitly unless you want to change them.
+            io.adapter(sio_redis({ host: 'localhost', port: 6379 }));
+
+            // Here you might use Socket.IO middleware for authorization etc.
+
+            // Listen to messages sent from the master. Ignore everything else.
+            process.on('message', function (message, connection) {
+                if (message !== 'sticky-session:connection') {
+                    return;
+                }
+
+                // Emulate a connection event on the server by emitting the
+                // event with the connection the master sent us.
+                server.emit('connection', connection);
+            });
+        }
     } else {
-        io.listen(server);
-        return server;
+        var io;
+        if(config.port) {
+            io = socket();
+        } else {
+            var app = express();
+            var server = http.createServer(app);
+            io = socket(server);
+        }
+        var d = domain.create();
+
+        d.on('error', function(err) {
+            logger.error(err, 'Socket.io'+':'+config.port);
+        });
+
+        d.run(function() {
+            io.adapter(redis({ host: 'localhost', port: 6379 }));
+            io.on('connection', onConnect);
+        });
+        if(config.port) {
+            if(config.debug) {
+                logger.log('Sockets', ['green'], ' listening on port ', config.port, ['yellow'], '.');
+            }
+
+            io.listen(config.port, function() {
+                if(typeof callback=='function') cb();
+            });
+        } else {
+            io.listen(8888, function() {
+                if(typeof callback=='function') cb();
+            });
+            return server;
+        }
     }
 };
