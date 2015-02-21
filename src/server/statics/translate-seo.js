8751fe00b4002740d5b42230f5d34818c287b244
mokto <theo.mathieu1@gmail.com>
2015-02-21 22:20:35 +0100

    hotfix

--- src/server/statics/translate-seo.js
+++ src/server/statics/translate-seo.js
@@ -1,6 +1,6 @@
 module.exports = {
 	translate: function (str, lang) {
-		var trad = require(process.cwd()+'src/common/i18n/'+lang+'.json');
+		var trad = require(process.cwd()+'/src/common/i18n/'+lang+'.json');
 
 		var hashmap = [];
 		walk(trad, '', hashmap);
